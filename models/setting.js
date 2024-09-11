43683de2459b77c055862d424e47d1f866f11725
harisahmedjs <harisfreefire987@gmail.com>
2024-09-12 04:22:38 +0500

    backend bug fixed

--- models/setting.js
+++ models/setting.js
@@ -1,46 +1,18 @@
-const Settings = require('../models/settings');
+const mongoose = require("mongoose");
 
-exports.hideSummerCourses = async (req, res) => {
-  try {
-    let settings = await Settings.findOne();
-
-    if (!settings) {
-      settings = new Settings();
-    }
-
-    settings.hideSummerCourses = !settings.hideSummerCourses;
-    await settings.save();
-
-    // Respond with the updated settings
-    res.status(200).json({
-      status: 'success',
-      hideSummerCourses: settings.hideSummerCourses
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: 'fail',
-      message: error.message
-    });
+const settingsSchema = new mongoose.Schema({
+  hideSummerCourses: {
+    type: Boolean,
+    default: false,
   }
-}
-
-exports.getSiteSettings = async (req, res) => {
-  try {
-    let settings = await Settings.findOne();
-
-    if (!settings) {
-      settings = new Settings();
-      await settings.save();
-    }
-
-    res.status(200).json({
-      status: 'success',
-      data: { settings }
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: 'fail',
-      message: error.message
-    });
+},
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    timestamps: false,
+    versionKey: false,
   }
-}
+);
+
+module.exports = mongoose.model("Settings", settingsSchema);
